7384255e7ce74a0c5a09d4c9fa8cf16649b98a7a
James Tan <jamestansx@gmail.com>
2022-06-14 22:14:54 +0800

    Refactor main script

--- main.m
+++ main.m
@@ -1,12 +1,11 @@
 function db = main (varargin)
     close all;
 
-    global db last fignum; 
-    db = {};
+    db = cell();
     lastpart = 1;
     last = 1;
-    fignum = 1;
 
+    % Default Transfer Function
     % s = 1 / (s^2 + s + 1)
     dnum = [1];
     dden = [1 1 1];
@@ -22,7 +21,7 @@ function db = main (varargin)
 
 
     if nargin == 0 
-        part0;
+        part0();
         part1(dre,im); 
         part2(re, dim); 
         part3(wn, z);
@@ -31,21 +30,18 @@ function db = main (varargin)
         for i = 1:length (varargin)
             switch varargin{i}
                 case 0
-                    part0;
+                    part0();
                     continue;
                 case 1
                     part1(dre,im); 
                     continue
                 case 2
-                    lastpart = 2;
                     part2(re, dim); 
                     continue
                 case 3
-                    lastpart = 3;
                     part3(wn, z); 
                     continue
                 case 4
-                    lastpart = 4;
                     part4(w, zeta);
                     continue
                 otherwise
@@ -60,46 +56,50 @@ function db = main (varargin)
         fprintf(["Natural Frequency: ", num2str(wn), "\n"]);
         fprintf(["Complex pole: ", num2str(dre), " + ", "j", num2str(dim), "\n"]);
         T = initUnderdTf(dnum, dden);
-        figure(fignum);
+        f1 = figure;
         draw(T, 1);
-        fignum = fignum + 1;
         legend('s/(s^2 + s + 1)');
         title('Step Response of s/(s^2 + s + 1)');
-        figure(fignum);
+        f2 = figure;
         pmap(T, 1);
-        fignum = fignum + 1;
     end
 
     function part1 (a,b)
         discription(1, "Changes on the Imaginary Part of the Poles");
-        T = part(a, b, @setReIm);
-        drawenv(db{4,1});
+        [db{1},f1,f2] = part(a, b, @setReIm);
+        figure(f1);
+        hold all;
+        drawenv(db{1}{1,1});
         [str, i] = cLegend(b, '\omega_d');
-        figure(fignum - 1);
+        figure(f2);
         legend(str);
         str{i+1, 1} = "Envelope";
-        figure(fignum - 2);
+        figure(f1);
         legend(str);
         title("Step Response of Varying Imaginary Part of Poles");
-        for i = 1:1:4
-            fprintf([num2str(i), ") ", num2str(b(i)), "\n"]);
-            evalPerf(db{i,1});
+        j = 1;
+        for i = db{1}
+            fprintf([num2str(j), ") ", num2str(b(j)), "\n"]);
+            evalPerf(i{1});
             fprintf("***************\n");
+            j = j + 1;
         end
     end
 
     function part2 (a, b)
         discription(2, "Changes on the Real Part of the Poles");
-        T = part(a, b, @setReIm);
+        [db{2},f1,f2] = part(a, b, @setReIm);
         str = cLegend(a, '\alpha');
+        figure(f1);
         legend(str);
-        figure(fignum - 1);
+        figure(f2);
         legend(str);
-        figure(fignum - 2);
+        figure(f1);
         title("Step Response of Varying Real Part of Poles");
-        for i = 1:1:4
-            fprintf([num2str(i), ") ", num2str(a(i)), "\n"]);
-            D = db{i,2};
+        j = 1;
+        for i = db{2}
+            fprintf([num2str(j), ") ", num2str(a(j)), "\n"]);
+            D = i{1};
             n = D.num{1};
             d = D.den{1};
             evalPerf(D);
@@ -107,75 +107,77 @@ function db = main (varargin)
             fprintf(['damping ratio = ', num2str(x), "\n"]);
             fprintf(['natural frequency = ', num2str(y), "\n"]);
             fprintf("***************\n");
+            j = j + 1;
         end
     end
     
     function part3 (a, b)
         discription(3, "Changes on the damping ratio");
         str = cLegend(b, '\zeta');
-        T = part(a, b, @setWnRatio);
+        [db{3},f1,f2] = part(a, b, @setWnRatio);
+        figure(f1);
         legend(str);
-        figure(fignum - 1);
+        figure(f2);
         legend(str);
-        figure(fignum - 2);
+        figure(f1);
         title('Step Response of Varying Damping Ratio, \zeta of system');
-        for i = 1:1:4
-            fprintf([num2str(i), ") ", num2str(b(i)), "\n"]);
-            evalPerf(db{i,3});
+        j = 1;
+        for i = db{3}
+            fprintf([num2str(j), ") ", num2str(b(j)), "\n"]);
+            evalPerf(i{1});
             fprintf("***************\n");
+            j = j + 1;
         end
     end
 
     function part4 (a, b)
         discription(4, "Changes on the Natural Frequency");
-        T = part(a, b, @setWnRatio);
+        [db{4},f1,f2] = part(a, b, @setWnRatio);
         str = cLegend(a, '\omega_n');
+        figure(f1);
         legend(str);
-        figure(fignum - 1);
+        figure(f2);
         legend(str);
-        figure(fignum - 2);
+        figure(f1);
         title('Step Response of Varying Natural Frequency, \omega_n of system');
-        for i = 1:1:4
-            fprintf([num2str(i), ") ", num2str(a(i)), "\n"]);
-            evalPerf(db{i,4});
+        j = 1;
+        for i = db{4}
+            fprintf([num2str(j), ") ", num2str(a(j)), "\n"]);
+            evalPerf(i{1});
             fprintf("***************\n");
+            j = j + 1;
         end
     end
 
-    function T = part(a, b, setFunc)
+    function [D,f1,f2] = part(a, b, setFunc)
         if isscalar(a) const = a; j = b;
         else const = b; j = a;
         end
-
-        figure(fignum);
-        hold all;
+        
+        D  = cell();
+        f1 = figure;
+        f2 = figure;
 
         in = 1;
-        disp(j);
+        n = 1;
+
         for i = j
             if isscalar(a) [num, den] = setFunc(const, i);
             else [num, den] = setFunc(i, const);
             end
 
             T = initUnderdTf(num, den);
+            figure(f1);
+            hold all;
             draw(T, in);
-            figure(fignum + 1);
+            figure(f2);
             hold all;
             pmap(T, in);
-            figure(fignum);
             in = in + 1;
 
-            db{last, lastpart} = T;
-            last = last + 1;
+            D{n} = T;
+            n = n + 1;
         end
-
-        parthandler();
-    end
-
-    function parthandler
-        last = 1;
-        lastpart = lastpart + 1;
-        fignum = fignum + 2;
     end
     
     function iter = citer(first, last, len)
